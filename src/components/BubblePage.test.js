60640820007084d21d70d7f63b04c693f93795b0
M. Cullen McClellan <cullenmcclellan@gmail.com>
2021-02-22 02:56:03 -0500

    Nearly Complete

--- src/components/BubblePage.test.js
+++ src/components/BubblePage.test.js
@@ -1,55 +1,17 @@
 import React from "react";
 import { render, screen, waitFor } from "@testing-library/react";
 import BubblePage from "./BubblePage";
-import {getColorsWithAxios as mockGetColorsWithAxios} from '../helpers/getColorsWithAxios'
+import {ColorList as mockColorList} from './ColorList'
 
-jest.mock('../helpers/getColorsWithAxios')
+jest.mock('./ColorList')
 
-const mockColorData = {
-  data: [
-    {
-      color: "aliceblue",
-      code: { hex: "#f0f8ff"},
-      id: 1
-    },
-    {
-      color: "limegreen",
-      code: { hex: "#99ddbc"},
-      id: 2
-    },
-    {
-      color: "aqua",
-      code: {hex: "#00ffff"},
-      id: 3
-    }
-  ]
-}
 
 test("Renders BubblePage without errors", async () => {
   // Finish this test
-  mockGetColorsWithAxios.mockResolvedValueOnce(mockColorData);
-    const {rerender} = render(<BubblePage/>);
-    
-    await waitFor(() => {
-      rerender(<BubblePage/>); 
-    });
-    screen.debug();
-    const colorName = screen.getByText(/aliceblue/i);
-    expect(colorName).toBeInTheDocument(); 
 })
 
 test("Fetches data and renders the bubbles on mounting", async () => {
   // Finish this test
-  const errorMessage = {error: 'API is currently unavailable for color request'}
-  mockGetColors.mockRejectedValueOnce(errorMessage);
-  const {rerender} = render(<BubblePage/>);
-  
-  await waitFor(() => {
-    rerender(<BubblePage/>); 
-  });
-  screen.debug();
-  const colorName = screen.queryByText(/aqua/i);
-  expect(colorName).not.toBeTruthy(); 
 });
 
 //Task List
--- src/helpers/getColorsWithAxios.js
+++
@@ -1,7 +0,0 @@
-import { axiosWithAuth } from './axiosWithAuth';
-
-export const getColorsWithAxios = () => {
-    return axiosWithAuth().get('/colors')
-        .then(res => res)
-        .catch(error => error);
-  }
